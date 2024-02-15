d0650f62651974d109a934d8569258fdc635694e
tyfurrier <tyler.furrier@atscale.com>
2024-02-15 12:19:26 -0500

    words

--- loudness_calculator.py
+++ loudness_calculator.py
@@ -1,19 +1,90 @@
 from enum import Enum
-from typing import List, Tuple
+from typing import List, Tuple, Callable, Dict
+import librosa
 # citation: https://www.dsprelated.com/showcode/174.php
 
 class FScale(Enum):
     MEL = "mel"
     MIDI = "midi"
-    RAW = "raw"
+    HZ = "hz"
+
+    def to_hz(self, value: float):
+        to_hz_function: Dict[FScale: Callable] = {
+            FScale.MEL: librosa.mel_to_hz,
+            FScale.MIDI: librosa.midi_to_hz,
+            FScale.HZ: lambda x: x
+        }
+        return to_hz_function[self](value)
+
+    def to_mel(self, value: float):
+        return librosa.hz_to_mel(self.to_hz(value=value))
+
+    def to_midi(self, value: float):
+        return librosa.hz_to_midi(self.to_hz(value=value))
+
+
+
+def convert_pitch_unit(pitch: float, from_scale: FScale, to_scale: FScale) -> float:
+    conversion: Dict[FScale, Callable] = {
+        FScale.MEL: lambda x, value: x.to_mel(value),
+        FScale.HZ: lambda x, value: x.to_hz(value),
+        FScale.MIDI: lambda x, value: x.to_midi(value),
+    }
+    pitch = conversion[to_scale](from_scale, pitch)
+    return pitch
+
+
+FREQUENCIES_FROM_ISO_226 = [
+    20, 25, 31.5, 40, 50, 63, 80, 100, 125, 160, 200, 250, 315, 400, 500, 630, 800,
+         1000, 1250, 1600, 2000, 2500, 3150, 4000, 5000, 6300, 8000, 10000, 12500]
+ISO226_2023 = {
+        20: {"af": 0.635, "Lu": -31.5, "Tf": 78.1},
+        25: {"af": 0.602, "Lu": -27.2, "Tf": 68.7},
+        31.5: {"af": 0.569, "Lu": -23.1, "Tf": 59.5},
+        40: {"af": 0.537, "Lu": -19.3, "Tf": 51.1},
+        50: {"af": 0.509, "Lu": -16.1, "Tf": 44.0},
+        63: {"af": 0.482, "Lu": -13.1, "Tf": 37.5},
+        80: {"af": 0.456, "Lu": -10.4, "Tf": 31.5},
+        100: {"af": 0.433, "Lu": -8.2, "Tf": 26.5},
+        125: {"af": 0.412, "Lu": -6.3, "Tf": 22.1},
+        160: {"af": 0.391, "Lu": -4.6, "Tf": 17.9},
+        200: {"af": 0.373, "Lu": -3.2, "Tf": 14.4},
+        250: {"af": 0.357, "Lu": -2.1, "Tf": 11.4},
+        315: {"af": 0.343, "Lu": -1.2, "Tf": 8.6},
+        400: {"af": 0.330, "Lu": -0.5, "Tf": 6.2},
+        500: {"af": 0.320, "Lu": 0.0, "Tf": 4.4},
+        630: {"af": 0.311, "Lu": 0.4, "Tf": 3.0},
+        800: {"af": 0.303, "Lu": 0.5, "Tf": 2.2},
+        1000: {"af": 0.300, "Lu": 0.0, "Tf": 2.4},
+        1250: {"af": 0.295, "Lu": -2.7, "Tf": 3.5},
+        1600: {"af": 0.292, "Lu": -4.2, "Tf": 1.7},
+        2000: {"af": 0.290, "Lu": -1.2, "Tf": -1.3},
+        2500: {"af": 0.290, "Lu": 1.4, "Tf": -4.2},
+        3150: {"af": 0.289, "Lu": 2.3, "Tf": -6.0},
+        4000: {"af": 0.289, "Lu": 1.0, "Tf": -5.4},
+        5000: {"af": 0.289, "Lu": -2.3, "Tf": -1.5},
+        6300: {"af": 0.293, "Lu": -7.2, "Tf": 6.0},
+        8000: {"af": 0.303, "Lu": -11.2, "Tf": 12.6},
+        10000: {"af": 0.323, "Lu": -10.9, "Tf": 13.9},
+        12500: {"af": 0.354, "Lu": -3.5, "Tf": 12.3},
+    }
 
 def hearing_threshold(f: float):
-    """ Returns the hearing threshold in dB at a given frequency"""
-    if f == 1000:
-        return 2.4  # dB
+    """ Returns the hearing threshold in SPLdB at a given frequency"""
+    frequency_ends = _binary_search_boundaries(FREQUENCIES_FROM_ISO_226, f)
+
     else:
         raise NotImplementedError
 
+def _get_iso_value_for_f(f: float, value: str,
+                         scale: FScale = FScale.MEL) -> float:
+    """ Returns the inferred value (through interpolation) of a given key for a given
+    frequency in the ISO 226-2023 table"""
+    frequency_ends = _binary_search_boundaries(FREQUENCIES_FROM_ISO_226, f)
+    if frequency_ends[0] == frequency_ends[1]:
+        return ISO226_2023[frequency_ends[0]][value]
+    bottom_in_mel = frequency_ends[0]
+    return ISO226_2023[frequency_ends[0]][value]
 def _binary_search_boundaries(values: List[float], target: float) -> Tuple[float, float]:
     """ Returns the two values in an ordered list that encapsulate (min and max of)
     the given target. If the given target is in the list, it will return that value.
@@ -45,37 +116,7 @@ def db_from_phon(f: float, phon: float):
     Tf = [78.5, 68.7, 59.5, 51.1, 44.0, 37.5, 31.5, 26.5, 22.1, 17.9, 14.4,
           11.4, 8.6, 6.2, 4.4, 3.0, 2.2, 2.4, 3.5, 1.7 -1.3 -4.2,
           -6.0 -5.4 -1.5, 6.0, 12.6, 13.9, 12.3]
-    ISO226_2023 = {
-        20: {"af": 0.635, "Lu": -31.5, "Tf": 78.1},
-        25: {"af": 0.602, "Lu": -27.2, "Tf": 68.7},
-        31.5: {"af": 0.569, "Lu": -23.1, "Tf": 59.5},
-        40: {"af": 0.537, "Lu": -19.3, "Tf": 51.1},
-        50: {"af": 0.509, "Lu": -16.1, "Tf": 44.0},
-        63: {"af": 0.482, "Lu": -13.1, "Tf": 37.5},
-        80: {"af": 0.456, "Lu": -10.4, "Tf": 31.5},
-        100: {"af": 0.433, "Lu": -8.2, "Tf": 26.5},
-        125: {"af": 0.412, "Lu": -6.3, "Tf": 22.1},
-        160: {"af": 0.391, "Lu": -4.6, "Tf": 17.9},
-        200: {"af": 0.373, "Lu": -3.2, "Tf": 14.4},
-        250: {"af": 0.357, "Lu": -2.1, "Tf": 11.4},
-        315: {"af": 0.343, "Lu": -1.2, "Tf": 8.6},
-        400: {"af": 0.330, "Lu": -0.5, "Tf": 6.2},
-        500: {"af": 0.320, "Lu": 0.0, "Tf": 4.4},
-        630: {"af": 0.311, "Lu": 0.4, "Tf": 3.0},
-        800: {"af": 0.303, "Lu": 0.5, "Tf": 2.2},
-        1000: {"af": 0.300, "Lu": 0.0, "Tf": 2.4},
-        1250: {"af": 0.295, "Lu": -2.7, "Tf": 3.5},
-        1600: {"af": 0.292, "Lu": -4.2, "Tf": 1.7},
-        2000: {"af": 0.290, "Lu": -1.2, "Tf": -1.3},
-        2500: {"af": 0.290, "Lu": 1.4, "Tf": -4.2},
-        3150: {"af": 0.289, "Lu": 2.3, "Tf": -6.0},
-        4000: {"af": 0.289, "Lu": 1.0, "Tf": -5.4},
-        5000: {"af": 0.289, "Lu": -2.3, "Tf": -1.5},
-        6300: {"af": 0.293, "Lu": -7.2, "Tf": 6.0},
-        8000: {"af": 0.303, "Lu": -11.2, "Tf": 12.6},
-        10000: {"af": 0.323, "Lu": -10.9, "Tf": 13.9},
-        12500: {"af": 0.354, "Lu": -3.5, "Tf": 12.3},
-    }
+
     t_f = hearing_threshold(
         f)  # threshold of hearing in dB at frequency f based on ISO 226:2023
     t_r = hearing_threshold(1000)  # threshold of hearing in dB at 1000 Hz based on ISO 226:2023
--- test/test_loudness_calculator.py
+++ test/test_loudness_calculator.py
@@ -1,4 +1,9 @@
 from loudness_calculator import _binary_search_boundaries
+import pytest
+
+def test_pitch_conversion():
+    # todo
+    pass
 
 def test__binary_search_boundaries():
     f = [20, 25, 31.5, 40, 50, 63, 80, 100, 125, 160, 200, 250, 315, 400, 500, 630, 800,
@@ -6,3 +11,5 @@ def test__binary_search_boundaries():
     assert _binary_search_boundaries(f, 22.5) == (20, 25)
     assert _binary_search_boundaries(f, 12500) == (12500, 12500)
     # todo: test for 15khz, 12500khz, 20hz, and maybe 10 hz
+    with pytest.raises(Exception):
+        _binary_search_boundaries(f, 10)
