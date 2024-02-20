424051d446ce83b13c48422f51f5ab730230e1b4
Tyler Furrier <tyler.furrier@atscale.com>
2024-02-20 17:57:20 -0500

    pre generation commit

--- test/test_loudness_calculator.py
+++ test/test_loudness_calculator.py
@@ -5,14 +5,17 @@ import pytest
 from matplotlib import pyplot as plt
 import plotly.express as px
 
+
 def test_amplitude_to_db():
     assert round(lc.amplitude_from_db(6), 1) == 2  # doubling amplitude increases db by 6
     assert round(lc.amplitude_from_db(12), 1) == 4
     assert round(lc.amplitude_from_db(-50), 3) == 0.003
 
+
 def test_dummy():
     value = 10 ** (0.323 * (13.9 + (-10.9) / 10))
     assert value == 1.2499
+
 
 def test_db_of_f_at_phon():
     assert round(lc.db_from_phon(f=1000, phon=60), 4) == 60
@@ -20,8 +23,9 @@ def test_db_of_f_at_phon():
     assert round(db_10k_60, 4) == 73.2297
     assert round(lc.db_from_phon(f=440, phon=40), 4) == 44.2787
 
+
 @pytest.mark.skip
-def test_plot_ISO_interpolations():
+def test_plot_iso_interpolations():
     FREQUENCIES = [20, 25, 31.5, 40, 50, 63, 80, 100, 125, 160, 200, 250, 315, 400, 500, 630, 800,
          1000, 1250, 1600, 2000, 2500, 3150, 4000, 5000, 6300, 8000, 10000, 12500]
     for value in ["af", "Lu", "Tf"]:
--- tone_pair_creation.py
+++ tone_pair_creation.py
@@ -3,6 +3,8 @@ import loudness_calculator as lc
 import numpy as np
 from helpers import _prep_directory, _custom_write
 import os
+from typing import Tuple, List
+import pandas as pd
 
 def pure_tone_synthesizer(fundamental: int,
                           harmonic_decibels: list = None,
@@ -11,9 +13,9 @@ def pure_tone_synthesizer(fundamental: int,
                           length: int = 1,
                           normalize: bool = False,
                           amplitude_of_80dB_SPL: float = None,
-                          custom_minmax: tuple = None) -> np.ndarray:
+                          custom_minmax: tuple = None) -> Tuple[np.ndarray, int]:
     """ Returns one second of the fundamental and its harmonics at the given decibel SPL levels. The SPL is
-    calculated from an amplitude of 0.5 equating to a SPL of 80dB, the amplitude required to reach 80dB can be
+    calculated from an amplitude of 0.5 equating to an SPL of 80dB, the amplitude required to reach 80dB can be
     changed by setting the amplitude_of_80dB_SPL parameter.
     The returned wave is a numpy wave that has an amplitude range of -1 to 1 and is written as a
     32-bit floating-point sound file.
@@ -27,7 +29,7 @@ def pure_tone_synthesizer(fundamental: int,
         amplitude_of_80dB_SPL (float): See docstring
         custom_minmax (tuple)
         """
-    max_db = 0.5  # amplitude for 80dB pure tone so we can combine four without clipping
+    max_db = 0.5  # amplitude for 80dB pure tone, so we can combine four without clipping
     if amplitude_of_80dB_SPL is not None:
         max_db = amplitude_of_80dB_SPL
     if harmonic_decibels is None:
@@ -68,6 +70,37 @@ def create_test_tone(hz: float):
 def create_max_pairs(phon_levels: list = None):
     if phon_levels is None:
         phon_levels = [30, 40, 50, 60]
+    # read jake_pairs.csv into a pandas dataframe
+    jake_pairs: pd.DataFrame = pd.read_csv("jake_pairs.csv")
+    output_folder_path = os.path.join(__file__, os.pardir, "created_samples", "first_max_pairs")
+    for i, row in jake_pairs.iterrows():
+        for phon in phon_levels:
+            sounds_in_combination: List[Tuple[np.ndarray, int]] = []
+            for part in ["f1", "f2"]:
+                max_harmonic = int(row[f"{part} h3"])
+                decibel_list = [None for _ in range(max_harmonic)]
+                decibel_list[0] = lc.db_from_phon(f=row[part],
+                                                  phon=phon)
+                for h in ["h1", "h2", "h3"]:
+                    harmonic = int(row[f"{part} {h}"])
+                    hz_of_harmonic = row[part] * harmonic
+                    decibel_list[harmonic - 1] = lc.db_from_phon(f=hz_of_harmonic,
+                                                                 phon=phon)
+                wave, sr = pure_tone_synthesizer(fundamental=row[part],
+                                                 harmonic_decibels=decibel_list,
+                                                 length=1)
+                sounds_in_combination.append((wave, sr))
+            np.append(sounds_in_combination[0][0],
+                      np.append(np.zeros(sr // 2), sounds_in_combination[1][0]))
+            pair_folder = os.path.join(output_folder_path, row["Pair #"])
+            _prep_directory(default_path=pair_folder,
+                            clear_dir=True)
+            _custom_write(path=os.path.join(__file__, os.pardir, "created_samples",
+                                           f"{row['f1']}_{row['f2']}_{phon}phon.wav"),
+                          wave=sounds_in_combination[0][0],
+                          sr=sr,
+                          overwrite=True)
+
     
 if __name__ == "__main__":
     create_test_tone(1000)
